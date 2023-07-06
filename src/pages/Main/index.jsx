735dd0090733104160e0d48e4e74cb4175ae258e
Fabio <fabiomt.98@hotmail.com>
2023-07-06 18:32:49 -0300

    feat: button style

---
+++ src/pages/Main/index.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './style.css'
+
+function Main() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='container'>
+      <h1>hello</h1>
+    </div>
+  )
+}
+
+export default Main
